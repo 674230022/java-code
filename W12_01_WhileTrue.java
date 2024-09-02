e34a7dd0555526d9057e580f33be12c8e074d51a
674230022 <674230022@webmail.npru.ac.th>
2024-09-02 12:44:23 +0700

    Add files via upload

---
+++ W12_01_WhileTrue.java
@@ -0,0 +1,18 @@
+package W12;
+
+public class W12_01_WhileTrue {
+    public static void main(String[] args) {
+
+        int i = 1;
+        while (true) {
+            i++;
+            System.out.print(i+""); 
+            if(i>=1000){
+                 break; 
+            }  
+
+            
+
+            }
+    }
+}
---
+++ W12_02_WhileTrue_KB.java
@@ -0,0 +1,29 @@
+package W12;
+
+import java.util.Scanner;
+
+public class W12_02_WhileTrue_KB {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+
+        while(true){
+            System.err.print("Enter Number : ");
+            int Number = kb.nextInt();
+            if (Number ==0) break ;
+        }
+    
+    
+    
+    
+    
+    
+        // วนลูปไปเรื่อยๆ ใช้ while (true) 
+        // แสดงข้อความ Enter Number :
+        // รับค่าจากคีย์บอร์ดเก็บไว้ในตัวแปร number
+                
+        // ตรวจสอบเงื่อนไข ถ้า number เท่ากับ 0 ให้ออกจากลูป
+            // ออกจากลูป
+
+        
+    }
+}
